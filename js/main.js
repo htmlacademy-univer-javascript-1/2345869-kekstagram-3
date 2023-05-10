9c5c75772b4deee121720841e67ea0bc7ca02aef
Sol646 <solodovnik.in@edu.spbstu.ru>
2023-05-10 20:53:36 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-import { generatePhotosArray } from './data.js';
-import { renderPictures } from './render-pictures.js';
-import { PHOTOS_NUMBER } from './utils.js';
+//import { generatePhotosArray } from './data.js';
+import { dawnloadPictures } from './render-pictures.js';
 import { addUploaderListeners } from './uploader-form-controller.js';
 import { setupEffects } from './uploader-effects.js';
 
+dawnloadPictures();
 addUploaderListeners();
 setupEffects();
-renderPictures(generatePhotosArray(PHOTOS_NUMBER));
--- js/utils.js
+++
@@ -1,6 +0,0 @@
-const PHOTOS_NUMBER = 12;
-
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
-const getRandomNumber = (from, to) => Math.round(Math.random() * (to - from) + from);
-
-export {PHOTOS_NUMBER, checkStringLength, getRandomNumber};
